cb414326e774796fb6990c1cfeeb72a2d2caa5c5
gekofroot <user_example@email.com>
2022-04-19 14:35:30 -0600

    fresh commit

--- js/menu-color.js
+++ js/menu-color.js
@@ -3,7 +3,7 @@
    $(window).scroll(function(){
     if ($(this).scrollTop() > 350) {
        $('.icon-bar').addClass('opaque');
-       $('.navbar-brand img').attr('src', 'img/logo-light.png');
+       $('.navbar-brand img').attr('src', 'img/logo.png');
     } else {
        $('.icon-bar').removeClass('opaque');
        $('.navbar-brand img').attr('src', 'img/logo-light.png');
@@ -22,7 +22,7 @@ $(function(){
         if($(this).scrollTop() < 100) {
             $('#logo, #topbar, .cart-label').fadeIn('fast');
             $('.logo img')
-                .attr('src','img/logo-light.png');
+                .attr('src','img/logo.png');
         }
     });
 });
