38ef597075ba56ea4d4de50d45ce3ea11aa77e17
gekofroot <user_example@email.com>
2022-04-19 01:39:21 -0600

    fresh commit

--- js/menu-color.js
+++ js/menu-color.js
@@ -6,7 +6,7 @@
        $('.navbar-brand img').attr('src', 'img/logo.png');
     } else {
        $('.icon-bar').removeClass('opaque');
-       $('.navbar-brand img').attr('src', 'img/logo.png');
+       $('.navbar-brand img').attr('src', 'img/logo-light.png');
     }
 });
 
@@ -17,7 +17,7 @@ $(function(){
         if($(this).scrollTop() > 100) {
             $('#topbar, .cart-label').fadeOut('fast');
             $('.logo img')
-                .attr('src','img/logo.png');
+                .attr('src','img/logo-light.png');
         }
         if($(this).scrollTop() < 100) {
             $('#logo, #topbar, .cart-label').fadeIn('fast');
