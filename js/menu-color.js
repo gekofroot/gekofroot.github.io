dcbe8431f093118af9544977bf927b2664999088
gekofroot <user_example@email.com>
2022-04-19 14:17:29 -0600

    fresh commit

--- js/menu-color.js
+++ js/menu-color.js
@@ -3,7 +3,7 @@
    $(window).scroll(function(){
     if ($(this).scrollTop() > 350) {
        $('.icon-bar').addClass('opaque');
-       $('.navbar-brand img').attr('src', 'img/logo.png');
+       $('.navbar-brand img').attr('src', 'img/logo-light.png');
     } else {
        $('.icon-bar').removeClass('opaque');
        $('.navbar-brand img').attr('src', 'img/logo-light.png');
