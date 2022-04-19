ba22f3d0aae6060b7dbf2e62e5f4eaf166558a34
gekofroot <user_example@email.com>
2022-04-19 00:11:51 -0600

    fresh commit

--- js/menu-color.js
+++ js/menu-color.js
@@ -3,10 +3,10 @@
    $(window).scroll(function(){
     if ($(this).scrollTop() > 350) {
        $('.icon-bar').addClass('opaque');
-       $('.navbar-brand img').attr('src', 'img/logo-light.png');
+       $('.navbar-brand img').attr('src', 'img/logo.png');
     } else {
        $('.icon-bar').removeClass('opaque');
-       $('.navbar-brand img').attr('src', 'img/logo.png');
+       $('.navbar-brand img').attr('src', 'img/logo-light.png');
     }
 });
 
@@ -17,12 +17,12 @@ $(function(){
         if($(this).scrollTop() > 100) {
             $('#topbar, .cart-label').fadeOut('slow');
             $('.logo img')
-                .attr('src','img/logo.png');
+                .attr('src','img/logo-light.png');
         }
         if($(this).scrollTop() < 100) {
             $('#logo, #topbar, .cart-label').fadeIn('fast');
             $('.logo img')
-                .attr('src','img/logo-light.png');
+                .attr('src','img/logo.png');
         }
     });
 });
